f8b9bd928c5987731e5fff3f9060311036dd8073
Adam Heckler <aheckler@users.noreply.github.com>
2016-12-11 07:08:21 -0500

    Remove author byline

--- functions.php
+++ functions.php
@@ -7,8 +7,14 @@ function twentyseventeen_adam_theme_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'twentyseventeen_adam_theme_styles' );
 
-/* Add custom PHP below here! */
+// Add custom PHP below here!
 
+// Remove frontend edit links
 function twentyseventeen_edit_link() {
 	return '';
+}
+
+// Remove author byline
+function twentyseventeen_posted_on() {
+	echo '<span class="posted-on">' . twentyseventeen_time_link() . '</span>';
 }
